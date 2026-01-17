46057b30a9666ac96f3b8da119c6bf3f18da05b4
R-P28 <rpoulin016@gmail.com>
2026-01-17 09:38:33 -0500

    get message, return

--- backend/flaskServer.py
+++ backend/flaskServer.py
@@ -1,15 +1,24 @@
 from flask import Flask, request, jsonify
+from translator import translate_with_context
 
 app = Flask(__name__)
 
 
-@app.route("/examples")
-def examples():
-    return {"examples":["Example1", "Example2", "Example3"]}
+@app.route("/translate", methods = ["POST"])
+def translate():
+    data = request.get_json()
+    message = data.get("message")   # variable to confirm
+    language = data.get("language") #variable to confirm
+    
+    if not message or not language:
+        return jsonify({"error": "Missing 'message' or 'language' parameter"}), 400
 
-#@app.route("/translate", methods = ["POST"])
-#ef translate():
-    #message = request.get_json()
+
+    translated = translator.translate_with_context(message, language)
+    
+    return jsonify({
+       "translatedMessage": translated
+   })
 
 
 
