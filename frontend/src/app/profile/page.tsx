e7fe4a31c0c0ba49c6b066c3b5dd578915553734
varl0011 <varl0011@algonquinlive.com>
2026-01-17 13:23:21 -0500

    adds redirection

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -21,7 +21,7 @@ export default async function SignIn() {
       <form
         action={async () => {
           "use server";
-          await signIn("google");
+          await signIn("google", { redirectTo: '/secret'});
         }}
       >
         <button type="submit">Signin with Google</button>
