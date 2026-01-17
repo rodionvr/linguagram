b0c3100f119602e2036fb959f7aaf907ee1c508d
varl0011 <varl0011@algonquinlive.com>
2026-01-17 13:06:56 -0500

    Adds nextauth

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,15 +1,31 @@
+import { auth, signIn, signOut } from "@/auth";
 
-import { signIn } from "@/auth"
- 
-export default function SignIn() {
-  return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("google")
-      }}
-    >
-      <button type="submit">Signin with Google</button>
-    </form>
-  )
-} 
+export default async function SignIn() {
+  const session = await auth();
+  const user = session?.user;
+
+  return user ? (
+    <>
+      <h1>Welcome {user.name}</h1>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit" className="p-2 border-2 bl-blue-400">Signout</button>
+      </form>
+    </>
+  ) : (
+    <>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google");
+        }}
+      >
+        <button type="submit">Signin with Google</button>
+      </form>
+    </>
+  );
+}
